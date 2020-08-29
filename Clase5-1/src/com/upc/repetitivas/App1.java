6ac8fd6e081f1cf9f9f9a480774876876eaac1db
escorpio80@gmx.com <escorpio80@gmx.com>
2020-08-29 08:52:45 -0500

    02.02

--- Clase5-1/src/com/upc/repetitivas/App1.java
+++ Clase5-1/src/com/upc/repetitivas/App1.java
@@ -6,8 +6,7 @@ public class App1 {
         s = sumarNumerosNaturales(3);
         System.out.println(s);
     }
-     static int sumarNumerosNaturales(int n){
-        //1+2.....n --->i
+     static int sumarNumerosNaturales(int n){ //1+2.....n --->i
         int suma = 0;
         for(int i=1; i<=n ;i++){ //i++;   es lo mismo que ++i;  en cambio p=++i; es diferente que p=i++
             suma+=i;//1+2+3   suma+=i  es lo mismo que suma = suma + i (suma es acumulador acumula los valores de i)
