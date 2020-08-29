abda7ae3e8d8bf5c1ea97dcdb848c0620ff2fe81
escorpio80@gmx.com <escorpio80@gmx.com>
2020-08-29 08:45:07 -0500

    02.01

---
+++ Clase5-1/src/com/upc/repetitivas/App1.java
@@ -0,0 +1,17 @@
+package com.upc.repetitivas;
+
+public class App1 {
+    public static void main(String[] args) {
+        int s = 0;
+        s = sumarNumerosNaturales(3);
+        System.out.println(s);
+    }
+     static int sumarNumerosNaturales(int n){
+        //1+2.....n --->i
+        int suma = 0;
+        for(int i=1; i<=n ;i++){ //i++;   es lo mismo que ++i;  en cambio p=++i; es diferente que p=i++
+            suma+=i;//1+2+3   suma+=i  es lo mismo que suma = suma + i (suma es acumulador acumula los valores de i)
+        }
+        return suma;
+     }
+}
