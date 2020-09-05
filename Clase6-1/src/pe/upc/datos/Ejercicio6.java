b0a094470dbd6f3f0037065b7e5d77f4ef39d99a
escorpio80@gmx.com <escorpio80@gmx.com>
2020-09-05 12:11:08 -0500

    v8

---
+++ Clase6-1/src/pe/upc/datos/Ejercicio6.java
@@ -0,0 +1,57 @@
+package pe.upc.datos;
+
+import java.util.Scanner;
+
+public class Ejercicio6 {
+
+    public static void main(String[] args) { //Practika 6.0
+        Scanner sc = new Scanner(System.in);
+        double medicina;
+        int dias;
+        double porcentaje;
+
+        System.out.println("Ingresar la cantidad de medicina: ");
+        medicina = sc.nextDouble();
+
+        System.out.println("Ingresar el número de días: ");
+        dias = sc.nextInt();
+
+        System.out.println("Ingresar el % a eliminar: ");
+        porcentaje = sc.nextDouble();
+
+        //Para probar método medicinaEnElCuerpo
+        double medicinaRest = medicinaEnElCuerpo(medicina,dias);
+        System.out.printf("Después de %d se eliminará %.2f de medicina\n", dias,medicinaRest);
+
+        //Para probar método diasEnEliminarMedicina
+        int diasElim = diasEnEliminarMedicina(medicina,porcentaje);
+        System.out.printf("Tardará %d días en eliminar la medicina\n", diasElim);
+    }
+
+    static double medicinaEnElCuerpo(double medicina, int dias){
+        int contdias = 0;
+        double medicinaRes = medicina;
+
+        while (contdias < dias){
+            medicinaRes = medicinaRes - medicinaRes*0.20;
+            contdias += 1;
+        }
+        return medicinaRes;
+    }
+
+    static int diasEnEliminarMedicina(double medicina, double porcentaje){
+
+        double medicinaAEliminar = medicina * porcentaje / 100;
+        double sumaMedicinaAEliminar = 0.0;
+        int contdias = 0;
+        double aEliminar = 0.0;
+
+        while (medicinaAEliminar > sumaMedicinaAEliminar){
+            aEliminar = medicina * 0.20;
+            medicina = medicina - aEliminar;
+            sumaMedicinaAEliminar = sumaMedicinaAEliminar + aEliminar;
+            contdias += 1;
+        }
+        return contdias;
+    }
+}
---
+++ Clase6-1/src/pe/upc/datos/Test.java
@@ -0,0 +1,7 @@
+package pe.upc.datos;
+
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
